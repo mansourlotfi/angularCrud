eb2144af2873559c26f12b6b6d8dca3213d624a8
mansourlotfi <mansourlotfi@gmail.com>
2020-01-23 21:30:59 +0330

    read & create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'angularCrud';
+export class AppComponent implements OnInit {
+	title = 'angularCrud';
+	constructor(private http: HttpClient) {}
+
+	persons = [];
+
+	fetchdata = function() {
+		this.http.get('http://localhost:5555/persons').subscribe((res: Response) => {
+			console.warn('fired', res);
+			this.persons = res;
+		});
+	};
+
+	//add
+	personObj: object = {};
+	confirmationString: string = 'new person added';
+	isAdded: boolean = false;
+
+	addNewPerson = function(person) {
+		this.personObj = {
+			fName: person.fName,
+			lName: person.lName,
+			phoneNumber: person.phoneNumber,
+			adderss: person.adderss
+		};
+		this.http.post('http://localhost:5555/persons', this.personObj).subscribe((res: Response) => {
+			console.warn('post fired', res);
+			this.isAdded = true;
+			for (let i = 1; i < this.persons.length; i++) {
+				this.persons.push({
+					key: i,
+					fName: res[i].fName,
+					lName: res[i].lName,
+					phoneNumber: res[i].phoneNumber,
+					adderss: res[i].adderss
+				});
+			}
+		});
+	};
+
+	ngOnInit() {
+		this.fetchdata();
+		console.warn(this.persons, 'asd');
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	imports: [ BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
